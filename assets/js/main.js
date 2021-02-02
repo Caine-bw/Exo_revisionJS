48a826581ccf10f5a17c36f504b44ba1fec88f0a
Abdel <elb.abd@icloud.com>
2021-02-02 15:00:10 +0100

    Exo ForEach boucle 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,7 +203,7 @@ let c = [
 
 // c.forEach((element,index) => {
 //      if (index%2 == 1) {
-//          console.log(element); 
+//          console.log(element.toUpperCase()); 
 //      }
 //  });
 
@@ -234,9 +234,46 @@ let c = [
 
 // 6. Affiche tous les prénoms commencant par (a,e,m,f,y,n) mais pas les autres !
 
-c.forEach(element =>{
-    console.log(element)
-});
+// c.forEach((element)=>{
+// let e = element.charAt(0)
+// if ( e == "A" || e   == "E" || e   == "M" || e   == "F" || e   == "Y" || e   == "N") {
+//   console.log(element)
+// }
+// });
 
 
 // 7. Affiche tous les prénoms plus long que 5 caractères et commencant par (a,e,m,f,y,n) 
+
+// c.forEach((element)=>{
+//     let e = element.charAt(0).toLowerCase()
+//     if ( e == "a" || e   == "e" || e   == "m" || e   == "f" || e   == "y" || e   == "n" ) {
+//         if (element.length > 5) {
+            
+//             console.log(element)
+//         }
+//     }
+//     });
+
+
+
+
+
+//-------------------------------------------------------------------------------------/
+// ## Exo 3(boucle forEach) Partie 3
+// 1. Créer un tableau avec 9 prénoms
+
+
+
+// 2. Affiche tous les prénoms plus court que 5 caractère commencant par (a,e,m,f,y,n)  avec leur première lettre en majuscule
+
+
+
+// 14 Affiche tous les prénoms plus long que 4 caractères commencant par (k,c,m,f,y,n,a,e,p,o) et qui sont impaire dans la liste 
+
+
+
+// 3. Affiche tous les prénoms plus long que 5 caractères commencant par (k,c,m,f,y,n,a,e,p,o) et qui sont pair dans la liste avec leur premiere lettre en minuscule,leur seconde et derniere lettre en majuscule
+
+
+
+// 4. Affiche tous les prénoms plus court que 7 caractères commencant par (k,m,f,y,n,a,e,o) et qui sont pair dans la liste mais uniquement leur deux premieres lettres concaténé les uns aux aures dont la premiere lettre et en majuscule et la seconde en majuscule, exemple :  karima,fanny,nicolas,elisa qui donnera KaFaNiEl
